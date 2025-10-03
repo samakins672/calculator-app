81ce2e4c4d7f795346713cb6ce56cc0527b517dc
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2025-10-04 00:29:33 +0100

    Replace import.meta.env references via Babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
+const t = require('@babel/types');
+
+const replaceImportMetaEnv = () => ({
+  name: 'replace-import-meta-env',
+  visitor: {
+    MemberExpression(path) {
+      if (
+        path.get('object').matchesPattern('import.meta') &&
+        path.get('property').isIdentifier({ name: 'env' })
+      ) {
+        path.replaceWith(t.memberExpression(t.identifier('process'), t.identifier('env')));
+      }
+    },
+    OptionalMemberExpression(path) {
+      if (
+        path.get('object').matchesPattern('import.meta') &&
+        path.get('property').isIdentifier({ name: 'env' })
+      ) {
+        path.replaceWith(t.memberExpression(t.identifier('process'), t.identifier('env')));
+      }
+    },
+  },
+});
+
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [replaceImportMetaEnv],
   };
 };
